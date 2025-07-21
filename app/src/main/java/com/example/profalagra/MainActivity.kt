b2929da7d83068a25e79908a77a574d40b503b34
beldier <alvaro.yapu.cossio@gmail.com>
2025-07-20 21:58:21 -0400

    Add navigation fragments

--- app/src/main/java/com/example/profalagra/MainActivity.kt
+++ app/src/main/java/com/example/profalagra/MainActivity.kt
@@ -8,14 +8,81 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.profalagra.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val REQ_CODE_ASK_PERM = 111
+    private lateinit var binding: ActivityMainBinding
+
+    private val navGraphIds = listOf(
+        R.navigation.nav_list,
+        R.navigation.nav_create,
+        R.navigation.nav_settings
+    )
+
+    private val navHosts = mutableListOf<NavHostFragment>()
+    private var selectedIndex = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         solPerm()
+        setupBottomNavigation()
     }
+
+
+    private fun setupBottomNavigation() {
+        navGraphIds.forEachIndexed { index, navGraphId ->
+            val tag = "navHost_$index"
+            val existingFragment = supportFragmentManager.findFragmentByTag(tag) as? NavHostFragment
+
+            val navHostFragment = existingFragment ?: NavHostFragment.create(navGraphId).also {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.nav_host_container, it, tag)
+                    .hide(it)
+                    .commitNow()
+            }
+
+            navHosts.add(navHostFragment)
+        }
+
+        showFragment(0)
+
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            val index = when (item.itemId) {
+                R.id.home_button -> 0
+                R.id.add_button -> 1
+                R.id.settings_button -> 2
+                else -> 0
+            }
+
+            if (index != selectedIndex) {
+                showFragment(index)
+            }
+            true
+        }
+    }
+
+    private fun showFragment(index: Int) {
+        val transaction = supportFragmentManager.beginTransaction()
+        navHosts.forEachIndexed { i, fragment ->
+            if (i == index) transaction.show(fragment)
+            else transaction.hide(fragment)
+        }
+        transaction.commit()
+        selectedIndex = index
+    }
+
+    override fun onBackPressed() {
+        val currentNavController = navHosts[selectedIndex].navController
+        if (!currentNavController.popBackStack()) {
+            super.onBackPressed()
+        }
+    }
+
 
     //Metodo para dar permiso de leer archivos
     private fun solPerm() {
